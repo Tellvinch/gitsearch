86bc10e4635cf13be0ecc89db0f885bfafc3b6ef
Tellvinch <tellvinchimani@gmail.com>
2020-05-16 07:15:41 +0300

    add new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitsearchComponent } from './gitsearch/gitsearch.component';
 import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
+import { NavComponent } from './nav/nav.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitsearchComponent,
-    GitsearchFormComponent
+    GitsearchFormComponent,
+    NavComponent,
+    RepoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
