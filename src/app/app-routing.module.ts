e64827a9a77725259f80fc5f7dc9225e6edaef7a
Tellvinch <tellvinchimani@gmail.com>
2020-05-17 20:24:46 +0300

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
 
 
 
